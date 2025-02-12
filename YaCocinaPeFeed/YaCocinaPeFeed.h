2d41098eb7d332a402181a3564f5e5cc424a0320
PabloButron <lizardlabs@icloud.com>
2025-02-12 09:56:15 -0400

    Initial Commit

---
+++ YaCocinaPeFeed/YaCocinaPeFeed.h
@@ -0,0 +1,18 @@
+//
+//  YaCocinaPeFeed.h
+//  YaCocinaPeFeed
+//
+//  Created by Pablo Butron on 12/2/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YaCocinaPeFeed.
+FOUNDATION_EXPORT double YaCocinaPeFeedVersionNumber;
+
+//! Project version string for YaCocinaPeFeed.
+FOUNDATION_EXPORT const unsigned char YaCocinaPeFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YaCocinaPeFeed/PublicHeader.h>
+
+
