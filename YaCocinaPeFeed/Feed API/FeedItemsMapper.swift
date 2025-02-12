20315c9a7acd542ce8eb1ed443212385fb939007
PabloButron <lizardlabs@icloud.com>
2025-02-12 13:39:19 -0400

    Extract Feed Items Mapper to its own file

--- YaCocinaPeFeed/Feed API/FeedItemsMapper.swift
+++ YaCocinaPeFeed/Feed API/FeedItemsMapper.swift
@@ -1,5 +1,5 @@
 //
-//  RemoteFeedLoader.swift
+//  FeedItemsMapper.swift
 //  YaCocinaPeFeed
 //
 //  Created by Pablo Butron on 12/2/25.
@@ -7,45 +7,6 @@
 
 import Foundation
 
-public enum HTTPClientResult {
-    case success(Data, HTTPURLResponse)
-    case failure(Error)
-}
-
-public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
-}
-
-public final class RemoteFeedLoader {
-    private let url: URL
-    private let client: HTTPClient
-
-    public init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-
-    public enum Result: Equatable {
-        case success([FeedItem])
-        case failure(Error)
-    }
-
-    public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) { result in
-            switch result {
-            case .success(let data, let response):
-                completion(FeedItemsMapper.map(data, response: response))
-            case .failure:
-                completion(.failure(.connectivity))
-            }
-        }
-    }
-}
 
 class FeedItemsMapper {
 
@@ -112,5 +73,4 @@ class FeedItemsMapper {
         }
     }
 }
-
 
