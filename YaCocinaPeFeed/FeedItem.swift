9e1d4463dca115f21a34741aebe99e106287be65
PabloButron <lizardlabs@icloud.com>
2025-02-12 10:10:48 -0400

    Creates Model of FeedItem

---
+++ YaCocinaPeFeed/FeedItem.swift
@@ -0,0 +1,31 @@
+//
+//  FeedItem.swift
+//  YaCocinaPeFeed
+//
+//  Created by Pablo Butron on 12/2/25.
+//
+
+import Foundation
+
+struct FeedItem {
+    let idMeal: String
+    let strMeal: String
+    let strArea: String
+    let strDescription: String
+    let imageURL: URL
+    let strIngredient1: String
+    let strIngredient2: String
+    let strIngredient3: String
+    let strIngredient4: String
+    let strIngredient5: String
+    let strIngredient6: String
+    let strIngredient7: String
+    let strIngredient8: String
+    let strIngredient9: String
+    let strIngredient10: String
+    let strIngredient11: String
+    let strIngredient12: String
+    let strIngredient13: String
+    let strIngredient14: String
+    let strIngredient15: String
+    }
