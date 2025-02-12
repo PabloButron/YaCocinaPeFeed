6ccd4c83271ad0415fa2a9aaea3babf5f2ffeadd
PabloButron <lizardlabs@icloud.com>
2025-02-12 18:33:45 -0400

    API end to end test compares items received with harcoded items expected

--- YaCocinaPeFeedAPIEndToEndTests/YaCocinaPeFeedAPIEndToEndTests.swift
+++ YaCocinaPeFeedAPIEndToEndTests/YaCocinaPeFeedAPIEndToEndTests.swift
@@ -24,12 +24,42 @@ final class YaCocinaPeFeedAPIEndToEndTests: XCTestCase {
         wait(for: [exp], timeout: 10)
         
         switch receivedResult {
-        case .success(let feed):
-            XCTAssertEqual(feed.count, 1, "expected 8 items in the test accound feed")
+        case .success(let items):
+            XCTAssertEqual(items.count, 1, "expected 1 item in the test accound feed")
+            items.enumerated().forEach { index, item in
+                XCTAssertEqual(item, expectedItem(at: index))
+            }
+            
         case .failure(let error):
             XCTFail("expected success got \(error) instead")
+            
         default:
             XCTFail("expected success got no result instead")
         }
     }
+    //MARK: Helpers
+    
+    private func expectedItem(at index: Int) -> FeedItem {
+        return FeedItem(idMeal: "52771",
+                        strMeal: "Spicy Arrabiata Penne",
+                        strArea: "Italian",
+                        strDescription: "Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.",
+                        imageURL: URL(string: "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg")!,
+                        strIngredient1: "penne rigate",
+                        strIngredient2: "olive oil",
+                        strIngredient3: "garlic",
+                        strIngredient4: "chopped tomatoes",
+                        strIngredient5:  "red chilli flakes",
+                        strIngredient6: "italian seasoning",
+                        strIngredient7: "basil",
+                        strIngredient8: "Parmigiano-Reggiano",
+                        strIngredient9: "",
+                        strIngredient10: "",
+                        strIngredient11: "",
+                        strIngredient12: "",
+                        strIngredient13: "",
+                        strIngredient14: "",
+                        strIngredient15: "")
+    }
+    
 }
