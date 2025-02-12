d13d9e949e6ac55f57b25d9a82661b436c51a03d
PabloButron <lizardlabs@icloud.com>
2025-02-12 10:23:58 -0400

    Request data from url on load

--- YaCocinaPeFeedTests/RemoteFeedLoaderTests.swift
+++ YaCocinaPeFeedTests/RemoteFeedLoaderTests.swift
@@ -8,10 +8,21 @@
 import XCTest
 
 protocol HTTPClient {
-    
+    func get(from url: URL)
 }
 class RemoteFeedLoader {
+
+    let url: URL
+    let client: HTTPClient
+
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
     
+    func load () {
+        client.get(from: url)
+    }
 }
 
 
@@ -19,20 +30,35 @@ class RemoteFeedLoader {
 final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataUponInitialization () {
-        let (sut, client) = makeSUT ()
+        let (_, client) = makeSUT ()
         XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    func test_load_doesRequestDataFromURL () {
+        let url = URL(string: "https://a-url.com")!
+        let (sut, client) = makeSUT (url: url)
+        
+        sut.load ()
+        
+        XCTAssertEqual(client.requestedURLs, [url])
     }
     
     //MARK: Helpers
     
-    private func makeSUT () -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
-        let sut = RemoteFeedLoader ()
+    private func makeSUT (url: URL = URL(string: "https://any-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy ()
+        let sut = RemoteFeedLoader (url: url, client: client)
+        
         return (sut, client)
     }
 
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs: [URL] = []
+
+        func get(from url: URL) {
+            requestedURLs.append(url)
+        }
+        
         
     }
 }
