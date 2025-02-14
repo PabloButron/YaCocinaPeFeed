61a872dcd23209b84ec11138fecb0e15e716aadd
PabloButron <lizardlabs@icloud.com>
2025-02-14 12:16:50 -0400

    Creates logic to  show and hide search bar

--- YaCocinaPeFeediOSApp/YaCocinaPeFeediOSApp/ViewModels/HomeViewModel.swift
+++ YaCocinaPeFeediOSApp/YaCocinaPeFeediOSApp/ViewModels/HomeViewModel.swift
@@ -6,3 +6,66 @@
 //
 
 import Foundation
+import YaCocinaPeFeed
+
+enum SearchType {
+    case ingredient
+    case recipeName
+}
+
+class HomeViewModel: ObservableObject {
+    @Published var meals = [HomeItem]()
+    @Published var searchQuery: String = ""
+    @Published var searchType: SearchType = .ingredient
+    @Published var isNotFound: Bool = false
+
+
+    
+    
+    func searchMeals() async {
+        guard !searchQuery.isEmpty else { return }
+        let endpoint = searchType == .ingredient ?
+        "https://www.themealdb.com/api/json/v1/1/filter.php?i=\(searchQuery)" :
+        "https://www.themealdb.com/api/json/v1/1/search.php?s=\(searchQuery)"
+        
+        await fetchMeals(from: endpoint)
+    }
+    
+    private func fetchMeals(from urlString: String) async {
+        do {
+            guard let url = URL(string: urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!) else { return }
+            
+            let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+            let loader = RemoteHomeLoader(url: url, client: client)
+            
+            let items = try await withCheckedThrowingContinuation { continuation in
+                loader.load { result in
+                    switch result {
+                    case .success(let items):
+                        continuation.resume(returning: items)
+                    case .failure(let error):
+                        continuation.resume(throwing: error)
+                    @unknown default:
+                        print("Default case hit")
+                    }
+                }
+            }
+            DispatchQueue.main.sync {
+                if items.isEmpty {
+                    self.meals = []
+                    self.isNotFound = true
+                } else {
+                    self.meals = items
+                    self.isNotFound = false
+                }
+            }
+        } catch {
+            DispatchQueue.main.sync {
+                self.meals = []
+                self.isNotFound = true
+            }
+        }
+    }
+    
+}
+
--- YaCocinaPeFeediOSApp/YaCocinaPeFeediOSApp/Views/Home Views/HomeView.swift
+++ YaCocinaPeFeediOSApp/YaCocinaPeFeediOSApp/Views/Home Views/HomeView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct HomeView: View {
     @State private var isSearchVisible = false
+    @StateObject private var viewModel = HomeViewModel()
 
     
     
     var body: some View {
         NavigationView {
             VStack {
-                
+                if isSearchVisible {
+                    Picker("Search by", selection: $viewModel.searchType) {
+                        Text("Recipe Name").tag(SearchType.recipeName)
+                        Text("Ingredient").tag(SearchType.ingredient)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding()
+                    
+                    TextField(viewModel.searchType == .ingredient ? "Try: Chicken" : "Try: Pizza",
+                              text: $viewModel.searchQuery)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .onSubmit {
+                        Task {
+                            await viewModel.searchMeals()
+                        }
+                    }
+                }
             }
             .toolbar {
                 ToolbarItem(placement: .topBarLeading) {
