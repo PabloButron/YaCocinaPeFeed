1289e6370b5ba18184848721249a5fd5646738e4
PabloButron <lizardlabs@icloud.com>
2025-02-14 11:54:03 -0400

    Creates Workspace And empty iOS App in order to develop iOS App

---
+++ YaCocinaPeFeediOSApp/YaCocinaPeFeediOSApp/YaCocinaPeFeediOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YaCocinaPeFeediOSAppApp.swift
+//  YaCocinaPeFeediOSApp
+//
+//  Created by Pablo Butron on 14/2/25.
+//
+
+import SwiftUI
+
+@main
+struct YaCocinaPeFeediOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
