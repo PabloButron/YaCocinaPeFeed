dac2d6181f17ee32fa69320bf97144171d0c66fa
PabloButron <lizardlabs@icloud.com>
2025-02-14 09:48:57 -0400

    Creates Home Item Model

---
+++ YaCocinaPeFeed/Feed Feature/Models/HomeItem.swift
@@ -0,0 +1,25 @@
+//
+//  HomeItem.swift
+//  YaCocinaPeFeed
+//
+//  Created by Pablo Butron on 13/2/25.
+//
+
+import Foundation
+
+public struct HomeItem: Identifiable {
+    public let idMeal: String
+    public let strMeal: String
+    public let strMealThumb: URL
+
+    public var id: String { 
+        return idMeal
+    }
+    
+    public init(idMeal: String, strMeal: String, strMealThumb: URL) {
+        self.idMeal = idMeal
+        self.strMeal = strMeal
+        self.strMealThumb = strMealThumb
+    }
+    
+}
